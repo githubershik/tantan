8ab61b519d43dbe4809e821c44a510b53ea76268
Kalyanshik Dima <chna170408@gmail.com>
2024-08-21 22:46:13 +0700

    час пилил красивую анимацию для кнопки

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-var button = document.getElementById("start");
-function start(){
-    alert("кнопку нажали")
+var btn = document.getElementById("start");
+var opacity = 100;
+async function start(){
+    for(let i = 0; i != 20; i++){
+        let promise = new Promise((resolve, reject) => {
+            setTimeout(() => resolve((opacity-=5).toString()+"%"), 40);
+          });
+        var res = await promise;
+        console.log(res);
+        btn.style.opacity = res.toString();
+        }
+        if (res == "0%") {
+            btn.style.display = "none";
+        }
 }
